3c9bf95d772a146f1112053fd4892ca11db4e51f
Gopala chachre <chachregopala@gmail.com>
2025-12-12 22:33:21 +0530

    first Program

--- 01_Basics/src/HelloWorld.java
+++ 01_Basics/src/HelloWorld.java
@@ -1,5 +1,20 @@
 public class HelloWorld {
     public static void main(String[] args) {
+
+        //This is my first Java Program
         System.out.println("Hello World!");
+        System.out.println("Good Morning!");
+
+        System.out.print("Hello World!\n");
+        System.out.print("Good Afternoon!\n");
+
+        System.out.print("Hello World!");
+        System.out.print("Good Night!");
+
+        /*
+            this
+            is a
+            multi-line comment
+         */
     }
 }
