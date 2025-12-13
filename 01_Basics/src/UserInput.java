b510814492debfb7a5e0cabe943b114970903fb9
Gopala chachre <chachregopala@gmail.com>
2025-12-13 15:10:58 +0530

    understanding the inputs

---
+++ 01_Basics/src/UserInput.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        //for strings
+        System.out.print("Enter your name:");
+        String name = input.nextLine();
+
+        System.out.print("Enter Father's name:");
+        String fatherName = input.nextLine(); //it will take only upto 1st space
+
+        // for integer
+        System.out.print("Enter your age :");
+        int age = input.nextInt();
+
+        // for double or float
+        System.out.print("Enter your GPA : ");
+        double gpa = input.nextDouble();
+
+        // for boolen
+        System.out.print("Are you a Student? (true/false): ");
+        boolean isStudent = input.nextBoolean();
+
+        System.out.println("Hello : " + name);
+        System.out.println("your Father's name : " + fatherName);
+        System.out.println("you are " + age + " Years old.");
+        System.out.println("your GPA : " + gpa);
+        System.out.println("Student : " + isStudent);
+
+        input.close();
+    }
+}
