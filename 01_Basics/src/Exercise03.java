596e064b3d30a31df9323f7cdcc4658838a14bb7
Gopala chachre <chachregopala@gmail.com>
2025-12-15 20:51:06 +0530

    Simple calculator

---
+++ 01_Basics/src/Exercise03.java
@@ -0,0 +1,33 @@
+// a normal calculator program
+
+import java.util.Scanner;
+
+public class Exercise03 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double num1, num2;
+        double result;
+        char operator;
+
+        System.out.print("Enter the first number: ");
+        num1 = input.nextDouble();
+
+        System.out.print("Enter an operator (+, -, *, /, ^): ");
+        operator = input.next().charAt(0);
+
+        System.out.print("Enter the second number: ");
+        num2 = input.nextDouble();
+
+        switch (operator){
+            case '+' -> System.out.printf("The resultant is : %.2f", num1 + num2);
+            case '-' -> System.out.printf("The resultant is : %.2f", num1 - num2);
+            case '*' -> System.out.printf("The resultant is : %.2f", num1 * num2);
+            case '/' -> System.out.printf("The resultant is : %.2f", num1 / num2);
+            case '^' -> System.out.printf("The resultant is : %.2f", Math.pow(num1, num2));
+            default -> System.out.println("You enter wrong operator!");
+        }
+
+        input.close();
+    }
+}
