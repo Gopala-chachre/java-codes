ad711a0735c533f954376c5186149f2dc151c42a
Gopala chachre <chachregopala@gmail.com>
2025-12-16 00:27:24 +0530

    understanding mathod overloading

---
+++ 03_OOPs/src/OverLoadingMethods.java
@@ -0,0 +1,26 @@
+public class OverLoadingMethods {
+    public static void main(String[] args) {
+
+        // overloaded methods = methods that share the same name,
+        //                      but different parameters
+        //                      signature = name + parameters
+
+        System.out.printf("a + b = %.2f\n", add(3,7));
+        System.out.printf("a + b + c = %.2f\n", add(3,7, 5));
+        System.out.printf("a + b + c + d = %.2f\n", add(3,7, 5, 9));
+
+    }
+
+    static double add(double a, double b){
+        return a + b;
+    }
+
+    static  double add(double a, double b, double c){
+        return a + b + c;
+    }
+
+    static  double add(double a, double b, double c, double d){
+        return a + b + c + d;
+    }
+
+}
