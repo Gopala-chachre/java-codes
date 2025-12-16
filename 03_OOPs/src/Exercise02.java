374cb93cc8542128fdac75e1169cb56de25f6f7e
Gopala chachre <chachregopala@gmail.com>
2025-12-16 22:24:06 +0530

    Banking program

---
+++ 03_OOPs/src/Exercise02.java
@@ -0,0 +1,82 @@
+// JAVA DICE ROLLER
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Exercise02 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        Random rand = new Random();
+        int numOfDice;
+        int total = 0;
+
+        System.out.print("Enter the No. of dice to Roll: ");
+        numOfDice = input.nextInt();
+
+        if (numOfDice > 0){
+
+            for (int i = 0; i < numOfDice; i++) {
+                int roll = rand.nextInt(1,7);
+                printDie(roll);
+                System.out.println("you roll: " + roll + "\n");
+                total += roll;
+            }
+
+            System.out.println("Total: " + total);
+
+        }else {
+            System.out.println("No. of dice must be greater then 0!");
+        }
+
+    }
+
+    static void printDie(int roll){
+        String dice1 = """
+                 -------
+                |       |
+                |   ⬤   |
+                |       |
+                 -------""";
+        String dice2 = """
+                 -------
+                |   ⬤   |
+                |       |
+                |   ⬤   |
+                 -------""";
+        String dice3 = """
+                 -------
+                | ⬤     |
+                |   ⬤   |
+                |     ⬤ |
+                 -------""";
+        String dice4 = """
+                 -------
+                | ⬤   ⬤ |
+                |       |
+                | ⬤   ⬤ |
+                 -------""";
+        String dice5 = """
+                 -------
+                | ⬤   ⬤ |
+                |   ⬤   |
+                | ⬤   ⬤ |
+                 -------""";
+        String dice6 = """
+                 -------
+                | ⬤   ⬤ |
+                | ⬤   ⬤ |
+                | ⬤   ⬤ |
+                 -------""";
+        switch (roll){
+            case 1 -> System.out.println(dice1);
+            case 2 -> System.out.println(dice2);
+            case 3 -> System.out.println(dice3);
+            case 4 -> System.out.println(dice4);
+            case 5 -> System.out.println(dice5);
+            case 6 -> System.out.println(dice6);
+        }
+
+    }
+
+}
