c01f8e1fc4bc7a02ebfa6aaa9b32c5e0c98e7058
Gopala chachre <chachregopala@gmail.com>
2025-12-25 13:50:21 +0530

    setup of slot machine

---
+++ 02_DifferentLib/src/SlotMachine.java
@@ -0,0 +1,26 @@
+// a slot machine program
+// user bet an amount and the slot will show using the emoji
+// if any matching pair or triple are there and user get the price money
+
+public class SlotMachine {
+    public static void main(String[] args) {
+        // Declare variables
+
+        // Display welcome message
+
+        // Play if balance > 0
+
+        // Enter bet amount
+        //      Verify if bet > balance
+        //      Verify if bet > 0
+        //      Subtract bet From balance
+
+        // Spin row
+        // Print row
+
+        // Get Payout
+        // ask to play again
+
+        // display Exit message
+    }
+}
