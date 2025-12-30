87952bc1ffdfde0a6dbf8ec4a944f6f9660c77c7
Gopala chachre <chachregopala@gmail.com>
2025-12-30 22:45:57 +0530

    to understanding OOP's

---
+++ 03_OOPs/src/OOPs.java
@@ -0,0 +1,11 @@
+public class OOPs { // class severs as the blueprints to creates objects
+    public static void main(String[] args) {
+
+        // Object = An entity that holds data(attributes) #eg -> name, age, height, weight
+        //          and can perform actions (methods) #eg -> eat, sleep
+        //          It is a reference data type
+
+        // to understand we made class name Car to understand it
+
+    }
+}
