ae4c31feb5bd889f05e97ef407c8de0ae2d4a8a5
Gopala chachre <chachregopala@gmail.com>
2025-12-30 22:37:30 +0530

    first understanding objects

---
+++ 03_OOPs/src/ObjectsInfo.java
@@ -0,0 +1,12 @@
+public class ObjectsInfo { // class severs as the blueprints to creates objects
+    public static void main(String[] args) {
+
+        // Object = An entity that holds data(attributes) #eg -> name, age, height, weight
+        //          and can perform actions (methods) #eg -> eat, sleep
+        //          It is a reference data type
+
+        // to understand we made class name Car to understand it
+
+
+    }
+}
