1927208eb4495b2991129953d02728fde02a8023
Gopala chachre <chachregopala@gmail.com>
2025-12-31 00:48:08 +0530

    using objects

--- 03_OOPs/src/Car.java
+++ 03_OOPs/src/Car.java
@@ -9,4 +9,22 @@ public class Car {
     double price = 58000.99;
     boolean isRunning = false;
 
+    void start(){
+        isRunning = true;
+        System.out.println("You Start the engine");
+    }
+
+    void stop(){
+        isRunning = false;
+        System.out.println("You Stop the engine");
+    }
+
+    void drive(){
+        System.out.println("You drive the " + model);
+    }
+
+    void brake(){
+        System.out.println("You brake the " + model);
+    }
+
 }
