e149353f286d4e2d699a668263a185ec69f543ec
Gopala chachre <chachregopala@gmail.com>
2025-12-30 23:15:14 +0530

    first understanding objects

--- 03_OOPs/src/OOPs.java
+++ 03_OOPs/src/OOPs.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class OOPs { // class severs as the blueprints to creates objects
     public static void main(String[] args) {
 
@@ -6,6 +9,20 @@ public class OOPs { // class severs as the blueprints to creates objects
         //          It is a reference data type
 
         // to understand we made class name Car to understand it
+        // to understand how to use object
+        Scanner input = new Scanner(System.in); // it is also an object
 
+        Car car = new Car();
+        System.out.println(car.model); // " . " dot (or dot operator) is used to access the thing within the object
+        System.out.println(car.make);
+        System.out.println(car.year);
+        System.out.println(car.price);
+        System.out.println(car.isRunning);
+
+        car.isRunning = true;
+        System.out.println(car.isRunning);
+
+
+        input.close();
     }
 }
