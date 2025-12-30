9848f331751e88162958fe06e92a52dd07cbc581
Gopala chachre <chachregopala@gmail.com>
2025-12-30 23:14:54 +0530

    using objects

---
+++ 03_OOPs/src/Car.java
@@ -0,0 +1,12 @@
+
+// a traditional way to understanding the OBJECTS in JAVA
+
+public class Car {
+
+    String make = "Ford";
+    String model = "Mustang";
+    int year = 1969;
+    double price = 58000.99;
+    boolean isRunning = false;
+
+}
