3b8ccd4321845b47dc58294d3f6468e822d38687
Gopala chachre <chachregopala@gmail.com>
2026-01-01 21:06:24 +0530

    going with constructors

--- 03_OOPs/src/Student.java
+++ 03_OOPs/src/Student.java
@@ -1,3 +1,18 @@
 public class Student {
     
+    String name;
+    int age;
+    double gpa;
+    boolean isEnrolled;
+
+    // automatically called when we create object
+    Student(String a, int age, double gpa){
+
+        this.name = a;  // this refers the object we're working on at that time
+        this.age = age;    // if we use student1.name the compiler got confuse
+        this.gpa = gpa;    // so we use the 'this' keyword to refers the current object
+        // and it is not compulsory to have the same name as at assigning
+
+    }
+    
 }
