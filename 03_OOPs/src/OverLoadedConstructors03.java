56177bf1a817c47d3e9f40f2d03fbca5d2de5e38
Gopala chachre <chachregopala@gmail.com>
2026-01-01 23:42:40 +0530

    how overloaded constructors works

--- 03_OOPs/src/OverLoadedConstructors03.java
+++ 03_OOPs/src/OverLoadedConstructors03.java
@@ -7,5 +7,17 @@ public class OverLoadedConstructors03 {
 
         // to understand that me make a User class
 
+        User user1 = new User("John wick");
+        System.out.println(user1.userName + " " + user1.email + " " + user1.age);
+
+        User user2 = new User("Mike Wheeler", "mikewheeler11@gmail.com");
+        System.out.println(user2.userName + " " + user2.email + " " + user2.age);
+
+        User user3 = new User("Will", "willthesorsorer@gmail.com", 17);
+        System.out.println(user3.userName + " " + user3.email + " " + user3.age);
+
+        User user4 = new User();
+        System.out.println(user4.userName + " " + user4.email + " " + user4.age);
+
     }
 }
