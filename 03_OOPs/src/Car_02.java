60b589e2cdd3859deeec60d360a9d387a8803675
Gopala chachre <chachregopala@gmail.com>
2026-01-02 00:04:26 +0530

    working class

---
+++ 03_OOPs/src/Car_02.java
@@ -0,0 +1,15 @@
+public class Car_02 {
+
+    String model;
+    String color;
+
+    Car_02(String model, String color){
+        this.model = model;
+        this.color = color;
+    }
+
+    void drive(){
+        System.out.println("you drive the " + this.color + " " + this.model);
+    }
+
+}
