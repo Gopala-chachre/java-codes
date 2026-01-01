791e3a612abc2c0edb484009305f8756a4770b3d
Gopala chachre <chachregopala@gmail.com>
2026-01-01 20:23:28 +0530

    going with constructors

---
+++ 03_OOPs/src/Constructors02.java
@@ -0,0 +1,11 @@
+public class Constructors02 {
+    public static void main(String[] args) {
+
+        // CONSTRUCTOR = A spical method to initialize objects
+        //               You can pass arguments to a constructor
+        //               and set up initial values
+
+        // to understand that me make a student class
+
+    }
+}
