90708304c3b53e2302eee1716638cd5ef7f399ef
Gopala chachre <chachregopala@gmail.com>
2026-01-01 23:21:01 +0530

    how overloaded constructors works

---
+++ 03_OOPs/src/OverLoadedConstructors03.java
@@ -0,0 +1,11 @@
+public class OverLoadedConstructors03 {
+    public static void main(String[] args) {
+
+        // overloaded constructors = Allows a class to have multiple constructors
+        //                           with different parameters lists.
+        //                           Enables objects to be initialized in various ways.
+
+        // to understand that me make a User class
+
+    }
+}
