a8d50658ed9886d63903a7526ef9462ffe8c4584
Gopala chachre <chachregopala@gmail.com>
2026-01-02 00:12:54 +0530

    how to make array of object

--- 03_OOPs/src/ArrayOfObjects.java
+++ 03_OOPs/src/ArrayOfObjects.java
@@ -8,11 +8,32 @@ public class ArrayOfObjects {
         Car_02 car3 = new Car_02("Mini-cooper", "yellow-white");
 
         // Car_02[] cars = new Car[3];
-        Car_02[] cars = {car1, car2, car3};
+        Car_02[] cars1 = {car1, car2, car3};
 
-        for (Car_02 car : cars) {
+        for (Car_02 car : cars1) {
             car.drive();
         }
+
+        // or we can do this
+        // it is known as anonymous objects
+        Car_02[] cars2 = {new Car_02("Mustang", "black"),
+                          new Car_02("Ferrari", "Red"),
+                          new Car_02("Mini-cooper", "yellow-white")};
+
+        System.out.println();
+        for (Car_02 car : cars2) {
+            car.drive();
+        }
+        
+        for (Car_02 car : cars2){
+            car.color = "black";
+        }
+
+        System.out.println();
+        for (Car_02 car : cars2) {
+            car.drive();
+        }
+
 
     }
 }
