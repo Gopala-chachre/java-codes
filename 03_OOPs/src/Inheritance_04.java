172572d92c02ecde87b41e4f9d697ebc5dfbac32
Gopala chachre <chachregopala@gmail.com>
2026-01-02 23:52:25 +0530

    learning working of inheritance

---
+++ 03_OOPs/src/Inheritance_04.java
@@ -0,0 +1,11 @@
+public class Inheritance_04 {
+    public static void main(String[] args) {
+
+        // Inheritance = One class inherits the attributes and methods
+        //               from another class.
+        //               Child <- Parent
+
+        
+
+    }
+}
