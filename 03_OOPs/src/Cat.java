72e5f154d75d98d722f338d613cf8c44baa13fdc
Gopala chachre <chachregopala@gmail.com>
2026-01-03 00:09:32 +0530

    to inheritance

--- 03_OOPs/src/Cat.java
+++ 03_OOPs/src/Cat.java
@@ -1,4 +1,11 @@
 public class Cat extends Animal{
     // even after tha class does not have anything in them,
     // they will have the inherits the Animal class
+
+    //the child have their own attributes and methods
+    int lives = 9;
+
+    void speak(){
+        System.out.println("The cat goes | meow |");
+    }
 }
--- 03_OOPs/src/Dog.java
+++ 03_OOPs/src/Dog.java
@@ -1,4 +1,12 @@
 public class Dog extends Animal{
     // even after tha class does not have anything in them,
     // they will have the inherits the Animal class
+
+
+    //the child have their own attributes and methods
+    int lives = 1;
+
+    void speak(){
+        System.out.println("The dog goes | Woof |");
+    }
 }
