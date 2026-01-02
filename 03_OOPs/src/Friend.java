44920f48df8ab1c38b23f167ff5ec63ef31d54b2
Gopala chachre <chachregopala@gmail.com>
2026-01-02 22:33:58 +0530

    for static understanding

---
+++ 03_OOPs/src/Friend.java
@@ -0,0 +1,17 @@
+public class Friend {
+
+    // if there is no static then every object have their own copy of this int variable
+    static int numOfFriends; // by the keyword it will share the same int variable in both the objects
+    String name;
+
+    Friend(String name){
+        this.name = name;
+        numOfFriends++;
+    }
+
+    static void showFriends(){
+        System.out.println("You have " + numOfFriends + " Friends"); // hear we didn't need the 'this' keyword because
+                                                                     // the numOfFriends belongs to class not to any object
+    }
+
+}
---
+++ 03_OOPs/src/StaticKeyword_03.java
@@ -0,0 +1,20 @@
+public class StaticKeyword_03 {
+    public static void main(String[] args) {
+
+        // static = Makes a variable or method belong to the class
+        //          rather than to any specific object.
+        //          Commonly used for utility methods or shared resources
+
+        // to understand we made class name 'Friend' to understand it
+
+        Friend friend1 = new Friend("John wick");
+        System.out.println(friend1.numOfFriends); // it is also ok but
+
+        Friend friend2 = new Friend("Mike");
+        System.out.println(Friend.numOfFriends); // to use the static attribute or
+                                                 // method we prefer to use the class name instead of the object name
+
+        Friend.showFriends();
+
+    }
+}
