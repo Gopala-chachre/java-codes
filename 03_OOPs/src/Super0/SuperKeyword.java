d885edc9f614d609de0ea6d80d2d360a42152a06
Gopala chachre <chachregopala@gmail.com>
2026-01-03 16:13:43 +0530

    understanding super keyword

--- 03_OOPs/src/Super0/SuperKeyword.java
+++ 03_OOPs/src/Super0/SuperKeyword.java
@@ -1,4 +1,22 @@
 package Super0;
 
 public class SuperKeyword {
+    public static void main(String[] args) {
+
+        // super = Refers to the parent class (subclass <- superclass)
+        //         Used in constructors and method overloading
+        //         calls the parent constructor to initialize attributes
+
+        // to understand we made few different class (Person)
+
+        Person person1 = new Person("John", "Wick");
+
+        person1.showName();
+
+        Student student = new Student("Mike", "Weeler", 7.7);
+        student.showName();
+
+        System.out.println(student.gpa);
+
+    }
 }
