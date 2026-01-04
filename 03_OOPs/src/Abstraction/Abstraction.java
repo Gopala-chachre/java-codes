c823b0195f7bfdb6b44ec1abf5e31dded19c95a5
Gopala chachre <chachregopala@gmail.com>
2026-01-04 22:08:11 +0530

    working of abstraction classes

---
+++ 03_OOPs/src/Abstraction/Abstraction.java
@@ -0,0 +1,19 @@
+package Abstraction;
+
+public class Abstraction {
+    public static void main(String[] args) {
+
+        // abstract = Used to define abstract classes and methods.
+        //            Abstraction is the process of hiding implementation details
+        //            and showing only the essential features.
+        //            Abstract classes CAN'T be instantiated directly
+        //            Can contain 'abstract' methods (which must be implemented)
+        //            Can contain 'concrete' methods (which are inherited)
+
+        // For Example = When we go for learning a CAR we just go for the ACCELERATOR ,BRAKE & GEARS
+        //               not for how Engine works.
+
+
+
+    }
+}
