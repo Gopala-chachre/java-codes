ef21321c229015d7c5611312e1407e6d3328991d
Gopala chachre <chachregopala@gmail.com>
2026-01-04 22:51:21 +0530

    for abstraction

--- 03_OOPs/src/Abstraction/Shape.java
+++ 03_OOPs/src/Abstraction/Shape.java
@@ -4,4 +4,10 @@ package Abstraction;
 
 public abstract class Shape {
 
+    abstract double area(); // ABSTRACT
+
+    void display(){ // CONCRETE
+        System.out.println("This is a Shape.");
+    }
+
 }
