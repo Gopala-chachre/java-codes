cc8d226c1a7d018218ea4ba7f20154d8538ed705
Gopala chachre <chachregopala@gmail.com>
2026-01-05 18:20:35 +0530

    Understanding Composition in java

---
+++ 03_OOPs/src/Composition/Composition.java
@@ -0,0 +1,25 @@
+package Composition;
+
+public class Composition {
+    public static void main(String[] args) {
+
+        // Composition = Represents a "part-of" relationship between objects.
+        //               For example, an Engine is "part of" car.
+        //               Allows complex object to be constructed from smaller objects.
+
+        Car car = new Car("Mastung", 1969, "V8");
+
+        System.out.println(car.model);
+        System.out.println(car.year);
+        System.out.println(car.engine); // Composition
+        // so we go with ->
+        System.out.println(car.engine.type); // the '.' is an access modifier
+
+        System.out.println();
+        car.start();
+
+        // if we delete the car object that should also delete our engine object because it is a part of it.
+        // and we no longer have the access to the engine.
+
+    }
+}
